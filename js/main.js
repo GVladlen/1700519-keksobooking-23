ab536f8b47c0e2166cdab7f4bd922a9c5a59f315
GVladlen <vladgyl@yandex.ru>
2021-06-02 06:31:46 +0800

    Вспомогательные функции

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//возвращает целое число из диапазона
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+getRandomInt(0, 100);
+//console.log(getRandomInt(0, 100 ).toFixed(3));
+
+// число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
+
+function getRandomNum(min, max) {
+  return Math.random() * (max - min + 1) + min;
+}
+getRandomNum(0, 100 ).toFixed(3);
+//console.log(getRandomNum(0, 100 ).toFixed(3));
