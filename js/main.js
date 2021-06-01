4cee91cca1f7fb5b1d25a560da694f829214a2e0
GVladlen <vladgyl@yandex.ru>
2021-06-02 07:06:27 +0800

    подправил

--- js/main.js
+++ js/main.js
@@ -3,12 +3,12 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomInt(0, 100);
-//console.log(getRandomInt(0, 100 ).toFixed(3));
+//console.log(getRandomInt(0, 100 );
 
 // число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
 
 function getRandomNum(min, max) {
   return Math.random() * (max - min + 1) + min;
 }
-getRandomNum(0, 100 ).toFixed(3);
+getRandomNum((0, 100 ).toFixed(3));
 //console.log(getRandomNum(0, 100 ).toFixed(3));
