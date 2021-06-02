effee0b1cacbbaec8a0e0c637ce33afaa2f3016a
GVladlen <vladgyl@yandex.ru>
2021-06-02 15:19:30 +0800

    Исправил

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 //возвращает целое число из диапазона
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+  return (Math.floor(Math.random() *(max - min +1) + min));
 }
-getRandomInt(0, 100);
-//console.log(getRandomInt(0, 100 );
+getRandomInt(300, 0);
+
 
 // число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
 
-function getRandomNum(min, max) {
-  return Math.random() * (max - min + 1) + min;
+
+function getRandomNum(min, max, comma) {
+  return (Number((Math.random() * (max - min) + min).toFixed(comma)));
 }
-getRandomNum((0, 100 ).toFixed(3));
-//console.log(getRandomNum(0, 100 ).toFixed(3));
+getRandomNum(100, 100, 4);
